8fb35a78490096f20ff0c0557387ab11875eebdc
ginkgo <yaoxiaoming@jk.cn>
2018-07-31 17:53:09 +0800

    setting timeout

--- staging/src/k8s.io/apiserver/pkg/server/config.go
+++ staging/src/k8s.io/apiserver/pkg/server/config.go
@@ -258,7 +258,7 @@ func NewConfig(codecs serializer.CodecFactory) *Config {
 		EnableMetrics:                true,
 		MaxRequestsInFlight:          400,
 		MaxMutatingRequestsInFlight:  200,
-		RequestTimeout:               time.Duration(60) * time.Second,
+		RequestTimeout:               time.Duration(5) * time.Minute, //time.Duration(60) * time.Second,
 		MinRequestTimeout:            1800,
 		EnableAPIResponseCompression: utilfeature.DefaultFeatureGate.Enabled(features.APIResponseCompression),
 
